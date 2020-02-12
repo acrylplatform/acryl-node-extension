b70b97c622699e30821877b6dc6588a20e3c062a
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2020-02-12 16:39:39 +0700

    Fix settings implicit

--- src/main/scala/com/acrylplatform/extensions/node/Settings.scala
+++ src/main/scala/com/acrylplatform/extensions/node/Settings.scala
@@ -3,7 +3,7 @@ package com.acrylplatform.extensions.node
 import com.typesafe.config.Config
 import net.ceedubs.ficus.Ficus._
 import net.ceedubs.ficus.readers.ArbitraryTypeReader._
-import net.ceedubs.ficus.readers.ValueReader
+import net.ceedubs.ficus.readers.{NameMapper, ValueReader}
 
 case class Settings(
     webhook: WebhookSettings,
@@ -25,8 +25,10 @@ case class NotificationsSettings(
 )
 
 object Settings {
+  implicit val chosenCase: NameMapper = net.ceedubs.ficus.readers.namemappers.implicits.hyphenCase
+
   implicit val valueReader: ValueReader[Settings] =
-    (cfg: Config, path: String) => fromConfig(cfg.getConfig(path))
+    (cfg, path) => fromConfig(cfg.getConfig(path))
 
   private[this] def fromConfig(config: Config): Settings = {
     val webhookSettings       = config.as[WebhookSettings]("webhook")
@@ -36,6 +38,40 @@ object Settings {
       webhook = webhookSettings,
       blockUrl = blockUrl,
       notifications = notificationsSettings
+    )
+  }
+}
+
+object WebhookSettings {
+  implicit val valueReader: ValueReader[WebhookSettings] =
+    (cfg, path) => fromConfig(cfg.getConfig(path))
+
+  private[this] def fromConfig(config: Config): WebhookSettings = {
+    val url     = config.as[String]("url")
+    val method  = config.as[String]("method")
+    val headers = config.as[Seq[String]]("headers")
+    val body    = config.as[String]("body")
+    WebhookSettings(
+      url = url,
+      method = method,
+      headers = headers,
+      body = body
+    )
+  }
+}
+
+object NotificationsSettings {
+  implicit val valueReader: ValueReader[NotificationsSettings] =
+    (cfg, path) => fromConfig(cfg.getConfig(path))
+
+  private[this] def fromConfig(config: Config): NotificationsSettings = {
+    val startStop     = config.as[Boolean]("start-stop")
+    val acrylReceived = config.as[Boolean]("acryl-received")
+    val leasing       = config.as[Boolean]("leasing")
+    NotificationsSettings(
+      startStop = startStop,
+      acrylReceived = acrylReceived,
+      leasing = leasing
     )
   }
 }
