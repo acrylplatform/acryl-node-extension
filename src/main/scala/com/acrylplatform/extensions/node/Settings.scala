fa54b74f24d5aff74fa29bf09105cd6b64578037
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2020-02-12 10:06:49 +0700

    Added files

---
+++ src/main/scala/com/acrylplatform/extensions/node/Settings.scala
@@ -0,0 +1,35 @@
+package com.acrylplatform.extensions.node
+
+import net.ceedubs.ficus.readers.ArbitraryTypeReader.arbitraryTypeValueReader
+import net.ceedubs.ficus.readers.ValueReader
+
+case class Settings(
+    webhook: WebhookSettings,
+    blockUrl: String,
+    notifications: NotificationsSettings
+)
+
+case class WebhookSettings(
+    url: String,
+    method: String,
+    headers: Seq[String],
+    body: String
+)
+
+case class NotificationsSettings(
+    startStop: Boolean,
+    acrylReceived: Boolean,
+    leasing: Boolean
+)
+
+object MinerNotifierSettings {
+  implicit val valueReader: ValueReader[Settings] = arbitraryTypeValueReader
+}
+
+object WebhookSettings {
+  implicit val valueReader: ValueReader[WebhookSettings] = arbitraryTypeValueReader
+}
+
+object NotificationsSettings {
+  implicit val valueReader: ValueReader[NotificationsSettings] = arbitraryTypeValueReader
+}
