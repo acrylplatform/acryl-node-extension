e5a10b289342060a5c9a955d0db5b7af2a751aee
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2020-02-18 14:43:20 +0700

    Added Status.scala

--- src/main/scala/com/acrylplatform/extensions/node/Logger.scala
+++ src/main/scala/com/acrylplatform/extensions/node/Logger.scala
@@ -6,22 +6,22 @@ import scalaj.http.Http
 import scala.util.control.NonFatal
 
 class Logger(settings: Settings) extends ScorexLogging {
-  private[this] def sendNotification(text: String): Unit =
+  private[this] def sendNotification(json: String): Unit =
     try {
       Http(settings.webhook.url)
         .headers(settings.webhook.headers.flatMap(s =>
           s.split(":") match {
             case Array(a, b) =>
-              Seq((a.trim, b.trim))
+              Seq(("Content-Type", "application/json"), (a.trim, b.trim))
             case _ =>
               log.error(s"""Can't parse "$s" header! Please check "webhook.headers" config. Its values must be in the "name: value" format""")
-              Seq()
+              Seq(("Content-Type", "application/json"))
         }))
-        .postData(settings.webhook.body.replaceAll("%s", text))
-        .method(settings.webhook.method)
+        .postData(json)
+        .method("POST")
         .asString
     } catch {
-      case NonFatal(e) => log.error("Logger: " + e.getMessage, e)
+      case NonFatal(e) => log.error(e.getMessage, e)
     }
 
   def info(message: String): Unit = {
