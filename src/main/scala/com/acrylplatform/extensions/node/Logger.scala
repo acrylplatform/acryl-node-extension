18958ef252b977cc434e7e64d4e10a57c00b151b
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2020-02-18 15:48:02 +0700

    Added Schema.scala

--- src/main/scala/com/acrylplatform/extensions/node/Logger.scala
+++ src/main/scala/com/acrylplatform/extensions/node/Logger.scala
@@ -1,11 +1,25 @@
 package com.acrylplatform.extensions.node
 
+import java.util.Date
+
+import com.acrylplatform.extensions.{Context => ExtensionContext}
+import com.acrylplatform.extensions.node.logger._
 import com.acrylplatform.utils.ScorexLogging
+import org.json4s.DefaultFormats
+import org.json4s.jackson.Serialization.write
 import scalaj.http.Http
 
 import scala.util.control.NonFatal
 
-class Logger(settings: Settings) extends ScorexLogging {
+class Logger(context: ExtensionContext, settings: Settings) extends ScorexLogging {
+
+  private[this] val address         = context.wallet.privateKeyAccounts.head.toAddress.toString
+  private[this] val nodeName        = context.settings.networkSettings.nodeName
+  private[this] val declaredAddress = context.settings.networkSettings.declaredAddress.toString
+  private[this] val version         = "1.0"
+
+  implicit val formats: DefaultFormats.type = DefaultFormats
+
   private[this] def sendNotification(json: String): Unit =
     try {
       Http(settings.webhook.url)
@@ -24,18 +38,59 @@ class Logger(settings: Settings) extends ScorexLogging {
       case NonFatal(e) => log.error(e.getMessage, e)
     }
 
+  private[this] def getTimestamp: Long = {
+    val date = new Date()
+    date.getTime
+  }
+
   def info(message: String): Unit = {
     log.info(message)
-    sendNotification(message)
+    val json = write(
+      Schema(address = address,
+             nodeName = nodeName,
+             declaredAddress = declaredAddress,
+             timestamp = getTimestamp,
+             typeData = "INFO",
+             version = version,
+             data = Info(message)))
+    sendNotification(json)
   }
 
   def warn(message: String): Unit = {
     log.warn(message)
-    sendNotification(message)
+    val json = write(
+      Schema(address = address,
+             nodeName = nodeName,
+             declaredAddress = declaredAddress,
+             timestamp = getTimestamp,
+             typeData = "WARN",
+             version = version,
+             data = Warn(message)))
+    sendNotification(json)
   }
 
   def err(message: String): Unit = {
     log.error(message)
-    sendNotification(message)
+    val json = write(
+      Schema(address = address,
+             nodeName = nodeName,
+             declaredAddress = declaredAddress,
+             timestamp = getTimestamp,
+             typeData = "ERROR",
+             version = version,
+             data = Error(message)))
+    sendNotification(json)
+  }
+
+  def statistics(statistics: Statistics): Unit = {
+    val json = write(
+      Schema(address = address,
+             nodeName = nodeName,
+             declaredAddress = declaredAddress,
+             timestamp = getTimestamp,
+             typeData = "STATISTICS",
+             version = version,
+             data = statistics))
+    sendNotification(json)
   }
 }
