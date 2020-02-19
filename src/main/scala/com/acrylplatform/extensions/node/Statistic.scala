d2d34b8654f088c3ad02767d9891d5bc2af0896a
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2020-02-19 17:36:12 +0700

    Added peers in Statistic.scala

--- src/main/scala/com/acrylplatform/extensions/node/Statistic.scala
+++ src/main/scala/com/acrylplatform/extensions/node/Statistic.scala
@@ -5,9 +5,34 @@ import com.acrylplatform.extensions.node.logger.Statistics
 import com.acrylplatform.extensions.{Context => ExtensionContext}
 import com.acrylplatform.features.BlockchainFeatures
 import com.acrylplatform.features.FeatureProvider._
+import org.json4s.JsonAST.{JArray, JString}
+import org.json4s.jackson.JsonMethods.parse
+import scalaj.http.{Http, HttpResponse}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
 
 class Statistic(context: ExtensionContext, settings: Settings) {
   private[this] val logger = new Logger(context, settings)
+
+  private[this] val localAPIAddress = context.settings.restAPISettings.bindAddress
+  private[this] val localAPIPort    = context.settings.restAPISettings.port
+
+  private[this] def getPeers: Future[Seq[String]] = Future {
+    val response: HttpResponse[String] = Http(s"http://$localAPIAddress:$localAPIPort/peers/connected").asString
+    val json                           = parse(response.body)
+    json \ "peers" match {
+      case JArray(list) =>
+        list.map(item => {
+          item \ "address" match {
+            case JString(s) => s.replaceAll("/", "")
+            case _          => ""
+          }
+        })
+      case _ => Seq.empty
+    }
+  }
 
   def start(): Unit = {
     val address         = context.wallet.privateKeyAccounts.head.toAddress
@@ -20,8 +45,6 @@ class Statistic(context: ExtensionContext, settings: Settings) {
 
     val version = Version.VersionString
 
-    val peers = Seq.empty
-
     val feature: Map[String, String] =
       (context.blockchain.featureVotes(heightLocal).keySet ++
         context.blockchain.approvedFeatures.keySet ++
@@ -30,15 +53,19 @@ class Statistic(context: ExtensionContext, settings: Settings) {
         "Feature: " + id.toString -> status.toString
       })(collection.breakOut)
 
-    logger.statistics(
-      Statistics(
-        balance = balance,
-        effectiveBalance = effectiveBalance,
-        effectiveBalanceNoConfirmations = effectiveBalanceNoConfirmations,
-        height = heightLocal,
-        version = version,
-        peers = peers,
-        feature = feature
-      ))
+    getPeers.onComplete {
+      case Success(peers) =>
+        logger.statistics(
+          Statistics(
+            balance = balance,
+            effectiveBalance = effectiveBalance,
+            effectiveBalanceNoConfirmations = effectiveBalanceNoConfirmations,
+            height = heightLocal,
+            version = version,
+            peers = peers,
+            feature = feature
+          ))
+      case Failure(_) => logger.err("Statistic: Local API request failed")
+    }
   }
 }
